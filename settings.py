b9010ad26efe0005efb0293e675d59e5d6b72eb0
LakshmiPrasannaKonudula <43558472+LakshmiPrasannaKonudula@users.noreply.github.com>
2019-07-31 00:52:27 -0400

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,12 @@
+TRACK_TERMS = ["#AOCResign", "#AOCisanidiot ", "#AOC", "  #ocasiocortez  "]
+CONNECTION_STRING = "sqlite:///tweets.db"
+CSV_NAME = "tweets.csv"
+TABLE_NAME = "election"
+TWITTER_APP_KEY="80eIs7jf6jW0WE04HG6cUBkZm"
+TWITTER_APP_SECRET="OkSfJTKmCuV8kPBR1Nnzywx8f0U8loGPkD9LqxjvQp8tGM2ngk"
+TWITTER_KEY="34148252-FEXkle1kFnPwusHmWFLMVFreS26fEei7nt7XHePSw"
+TWITTER_SECRET="XIj96npDmHV8aWGHbthX1tmHMJpyu7wUHuDdMHrvqbU7e"
+try:
+    from private import *
+except Exception:
+    pass
